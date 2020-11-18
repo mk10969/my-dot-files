16a6288d1ac1addf421bb4c2b1487738b9c726f4
mk10969 <mk10969@gmail.com>
2020-11-18 21:40:47 +0900

    update .config

---
+++ install.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+
+# universal variable
+set -Ux fish_user_paths /usr/local/bin $fish_user_paths
+set -Ux fish_user_paths /usr/local/sbin $fish_user_paths
+set -Ux fish_user_paths $HOME/.cargo/bin $fish_user_paths
+set -Ux fish_user_paths $HOME/.anyenv/bin $fish_user_paths
--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
-!# /bin/sh
+#!/bin/sh
 
-# Parameters
+##### Parameters #####
 pwd=$(pwd)
 
-# Functions
+##### Functions #####
 
 
-# Runs
-### xcode install
+##### Runs #####
+# xcode install
+echo "=========================================="
 echo "Setup install xcode..."
+echo "=========================================="
 if ! type xcode-select > /dev/null; then
     xcode-select --install
 fi
 
-### brew
+# install brew packages
+echo "=========================================="
 echo "Setup Homebrew and brew packages..."
-
+echo "=========================================="
 if ! type brew > /dev/null; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -25,22 +28,40 @@ if [ -e $pwd/Brewfile ]; then
     brew bundle
 fi
 
-
-### git
+# git config
+echo "=========================================="
 echo "Setup gitconfig create symlink..."
-
+echo "=========================================="
 ln -sf $pwd/gitconfig $HOME/.gitconfig
 ln -sf $pwd/gitignore_global $HOME/.gitignore_global
 
-### install
-echo "Setup install others..."
-##### rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-##### alacritty
+# install
+echo "=========================================="
+echo "Setup install terminal and languages..."
+echo "=========================================="
+## alacritty
 ghq get https://github.com/alacritty/alacritty.git
+## rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+## anyenv
+git clone https://github.com/anyenv/anyenv ~/.anyenv
 
+if [ ! -d $HOME/.anyenv/plugins ]; then
+    mkdir -p ~/.anyenv/plugins
+fi
 
+## anyenv plugins
+git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
+git clone https://github.com/znz/anyenv-git.git ~/.anyenv/plugins/anyenv-git
 
-~/.anyenv/bin/anyenv init
-set -Ux fish_user_paths $HOME/.cargo/bin $fish_user_paths
-set -Ux fish_user_paths $HOME/.anyenv/bin $fish_user_paths
+## init
+anyenv install --init
+## install **envs
+anyenv install nodenv
+anyenv install jenv
+anyenv install goenv
+anyenv install jlenv
+
+## rust packages
+cargo install xsv
+cargo install clipivot
