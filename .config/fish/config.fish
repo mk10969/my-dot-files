7f245ab8937458384a81761e8eae8609e0b40479
mk10969 <mk10969@gmail.com>
2021-01-21 20:15:00 +0900

    default python3.9

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,19 @@
-
 ### env path
 
 # homebrew
 set -x PATH /opt/homebrew/bin $PATH
-# arm64 golang (自前build)
+# golang arm64 (自前build)
 set -x PATH $HOME/.packages/go-darwin-arm64-bootstrap/bin $PATH
 set -x GOPATH $HOME/.go
 set -x PATH $GOPATH/bin $PATH
+# python3.9
+set -x PATH /opt/homebrew/opt/python@3.9/libexec/bin $PATH
+
 # gradle
 set -x PATH /opt/gradle/bin $PATH
 # flutter
 set -x PATH $HOME/ghq/github.com/flutter/flutter/bin $PATH
+
 
 ### alias
 alias ll='exa -aal --git'
