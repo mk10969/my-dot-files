da82f2186e3922803ff6a7d6559e33bbcf96d4de
mk10969 <mk10969@gmail.com>
2021-04-07 17:51:51 +0900

    go 入れ直し

--- .config/fish/conf.d/000-env.fish
+++ .config/fish/conf.d/000-env.fish
@@ -3,14 +3,12 @@
 # homebrew
 set -x PATH /opt/homebrew/bin $PATH
 
-# golang arm64 (自前build)
-set -x PATH $HOME/.packages/go-darwin-arm64-bootstrap/bin $PATH
+# golang
 set -x GOPATH $HOME/.go
 set -x PATH $GOPATH/bin $PATH
+
 # python3.9
 set -x PATH /opt/homebrew/opt/python@3.9/libexec/bin $PATH
 
 # gradle
 set -x PATH /opt/gradle/bin $PATH
-# flutter
-set -x PATH $HOME/ghq/github.com/flutter/flutter/bin $PATH
