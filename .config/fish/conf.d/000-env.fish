a755f9ad45642b44495e5181a7d8406b6963725d
mk10969 <mk10969@gmail.com>
2022-04-12 00:20:21 +0900

    fix

--- .config/fish/conf.d/000-env.fish
+++ .config/fish/conf.d/000-env.fish
@@ -3,12 +3,5 @@
 # homebrew
 set -x PATH /opt/homebrew/bin $PATH
 
-# golang
-set -x GOPATH $HOME/.go
-set -x PATH $GOPATH/bin $PATH
-
-# python3.9
-set -x PATH /opt/homebrew/opt/python@3.9/libexec/bin $PATH
-
 # gradle
 set -x PATH /opt/gradle/bin $PATH
